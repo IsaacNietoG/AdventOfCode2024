5d944d391e5b34f757a4b6830f9dd64222f874b1
mrtaichi <isaaniet@gmail.com>
2024-12-08 23:21:22 -0600

    Day 3 and begin of day 4

---
+++ day4/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+auto main(int argc, char *argv[]) -> int {
+    //STEP 1: Read input
+    string inputFile = "puzzleInput.txt";
+
+    return 0;
+}
